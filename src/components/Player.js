8b401790ee827640532090009043a861b642ef87
Elan Zinkow <ezinkow@gmail.com>
2021-02-09 15:47:33 -0600

    adjustments

--- src/components/Player.js
+++ src/components/Player.js
@@ -14,12 +14,12 @@ function Player(props) {
     return (
         <Col>
             <CardGroup>
-                <Card bg='warning'>
-                    <Card.Img variant="top" src={props.image} style={{ width: "100%", height: "200px", display: "block", margin: "auto"}}/>
-                    <Card.Title><h3><span style={{ color: "purple" }}>{props.firstName} {props.lastName}</span></h3></Card.Title>
+                <Card bg='warning' style={{borderRadius: "20px"}}>
+                    <Card.Img variant="top" src={props.image} style={{ width: "100%", height: "200px", display: "block", marginBottom: "10px", borderRadius: "20px"}}/>
+                    <Card.Title><h3 style={{ color: "purple", textAlign: "center"}}>{props.firstName} {props.lastName}</h3></Card.Title>
                     <Card.Body>
                         <Card.Text>
-                            <h3>Player Jersey: <span style={{ color: "purple" }}>{props.jerseyNumber}</span></h3>
+                            <h3>Player Jersey: <span style={{ color: "purple" }}>#{props.jerseyNumber}</span></h3>
                             <h3>Hobbies: </h3><p><span style={{ color: "purple" }}>{hobbies}</span></p>
                         </Card.Text>
                     </Card.Body>
